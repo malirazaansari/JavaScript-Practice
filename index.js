bb38145e2e2cbbe215c21d75cec0c8555f33a1b4
Ali raza <malirazaansari4@gmail.com>
2024-09-21 04:29:53 +0500

    if else and loops done

--- index.js
+++ index.js
@@ -50,32 +50,66 @@
 //Relational >,<,=<,=>
 //Equailty ===(both value and tupe of varible should be same),==,!==,!=
 
-//terniary and conditional
-let points = 110;
-let type = points > 100 ? "gold" : "silver";
-console.log(type);
+// //terniary and conditional
+// let points = 110;
+// let type = points > 100 ? "gold" : "silver";
+// console.log(type);
 
-//logical ope
-//AND(&&), OR(||), NOT(!)
-// logical operator without booleen
-//falsy values (undefined, Null, 0 , false(booleen flase), ''(empty string), NaN)
-let usercolor = "red"; //(if undefined then blue will show)
-let defaulatcolor = "blue";
-let color = usercolor || defaulatcolor;
+// //logical ope
+// //AND(&&), OR(||), NOT(!)
+// // logical operator without booleen
+// //falsy values (undefined, Null, 0 , false(booleen flase), ''(empty string), NaN)
+// let usercolor = "red"; //(if undefined then blue will show)
+// let defaulatcolor = "blue";
+// let color = usercolor || defaulatcolor;
 
-console.log(color);
+// console.log(color);
 
-//Bitwise operators
-// | , & ... for bit work 00000001, 00000010 etc
+// //Bitwise operators
+// // | , & ... for bit work 00000001, 00000010 etc
 
-//excercie
-//value swap
-let a = "red";
-let b = "blue";
+// //excercie
+// //value swap
+// let a = "red";
+// let b = "blue";
 
-let c = a;
-a = b;
-b = c;
+// let c = a;
+// a = b;
+// b = c;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+// //conditional statement
+// let role = "admin";
+
+// switch (role) {
+//   case "admin":
+//     console.log("admin");
+//     break;
+//   case "moderator":
+//     console.log("moderator");
+//     break;
+//   default:
+//     console.log("user");
+// }
+
+// if (role === "admin") console.log("admin");
+// else if (role === "moderator") console.log("moderator");
+// else console.log("user");
+
+// // for..in and for..of loops
+// const person = {
+//   name: "ali",
+//   age: 21,
+// };
+
+// for (let key in person) {
+//   console.log(key, ":", person[key]);
+// }
+
+// const Colors = ["yellow", "blue", "red"];
+
+// for (let color of Colors) {
+//   console.log(color);
+// }
