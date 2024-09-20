05ce57350d987b45eba45146c5ac5a3f2d3ea01c
Ali raza <malirazaansari4@gmail.com>
2024-09-21 03:54:37 +0500

    operators work done

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
-console.log("Hello world");
-//variables
-let fName = "ali";
-let lName = "raza";
+// console.log("Hello world");
+// //variables
+// let fName = "ali";
+// let lName = "raza";
 
-console.log(fName + " " + lName);
-//constraints let var const
-let name = "Ali Raza";
-let age = 21;
-let gender = true;
-//type of
-console.log(typeof name);
-console.log(typeof age);
-//object
-let person = {
-  name: "Ali Raza",
-  age: 21,
-};
+// console.log(fName + " " + lName);
+// //constraints let var const
+// let name = "Ali Raza";
+// let age = 21;
+// let gender = true;
+// //type of
+// console.log(typeof name);
+// console.log(typeof age);
+// //object
+// let person = {
+//   name: "Ali Raza",
+//   age: 21,
+// };
 
-person.name = "Ansari";
-person["age"] = 22;
+// person.name = "Ansari";
+// person["age"] = 22;
 
-console.log(person);
-//arrays
-let Colors = ["red", "green"];
-Colors[2] = "yellow";
+// console.log(person);
+// //arrays
+// let Colors = ["red", "green"];
+// Colors[2] = "yellow";
 
-console.log(Colors);
-console.log(Colors[0]);
-//function
-function greet(fName, lName) {
-  console.log("Hello " + fName + " " + lName);
-}
-greet("raza", "ali");
+// console.log(Colors);
+// console.log(Colors[0]);
+// //function
+// function greet(fName, lName) {
+//   console.log("Hello " + fName + " " + lName);
+// }
+// greet("raza", "ali");
 
-function square(num) {
-  return num * num;
-}
-console.log(square(2));
+// function square(num) {
+//   return num * num;
+// }
+// console.log(square(2));
+
+// operater
+
+//Arithmetic ope
+// +,-,*,/,%,**,++,--
+
+//Assingment ope
+//= and (= with all arithmetic operator)
+
+//Comparsion opr
+//Relational >,<,=<,=>
+//Equailty ===(both value and tupe of varible should be same),==,!==,!=
+
+//terniary and conditional
+let points = 110;
+let type = points > 100 ? "gold" : "silver";
+console.log(type);
+
+//logical ope
+//AND(&&), OR(||), NOT(!)
+// logical operator without booleen
+//falsy values (undefined, Null, 0 , false(booleen flase), ''(empty string), NaN)
+let usercolor = "red"; //(if undefined then blue will show)
+let defaulatcolor = "blue";
+let color = usercolor || defaulatcolor;
+
+console.log(color);
+
+//Bitwise operators
+// | , & ... for bit work 00000001, 00000010 etc
+
+//excercie
+//value swap
+let a = "red";
+let b = "blue";
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
