af2ddb932a43bc3ca6724bec783d656b8b177345
Ali raza <malirazaansari4@gmail.com>
2024-09-23 19:33:42 +0500

    functions excerises complete

---
+++ functionex.js
@@ -0,0 +1,22 @@
+//1st ex
+// function sum(...arr) {
+//   return arr.reduce((a, b) => a + b);
+//   //   if (arr.isArray([])) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+// console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// //2nd ex
+// const cricle = {
+//   radius: 2,
+//   get area() {
+//     return Math.PI * this.radius * this.radius;
+//   },
+// };
+// console.log(cricle.area);
+
+//3rd ex
