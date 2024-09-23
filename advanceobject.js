dc2843aa647f46d4c18fb9e6132ed45a252d53d9
Ali raza <malirazaansari4@gmail.com>
2024-09-24 03:05:24 +0500

    objects in advance course done

---
+++ advanceobject.js
@@ -0,0 +1,19 @@
+// //adsraction
+// //clouser
+// function Circle(radius) {
+//   this.radius = radius;
+
+//   let defaultLocation = { x: 0, y: 0 };
+//   let computeOptimumLocation = function (factor) {
+//     // ..
+//   };
+//   this.draw = function () {
+//     let x, y;
+//     computeOptimumLocation(0.1);
+//     this, radius;
+//     console.log("draw");
+//   };
+// }
+
+// const circle = new Circle(10);
+// circle.draw();
