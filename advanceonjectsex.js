28f2a0754daff8917a76c991adc42fb0b897a5b8
Ali raza <malirazaansari4@gmail.com>
2024-09-24 03:17:25 +0500

    excerise done of objects

---
+++ advanceonjectsex.js
@@ -0,0 +1,31 @@
+function Stopwatch(start, end, running, duration = 0) {
+  this.start = function () {
+    if (running) {
+      throw new Error("already start");
+    }
+    running = true;
+    start = new Date();
+  };
+  this.end = function () {
+    if (!running) {
+      throw new Error(`stopwatch isn't started yet`);
+    }
+    running = false;
+    end = new Date();
+
+    const seconds = (end.getTime() - start.getTime()) / 1000;
+
+    return (duration += seconds);
+  };
+  this.reset = function () {
+    start = null;
+    end = null;
+    running = false;
+    duration = 0;
+  };
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
+  });
+}
